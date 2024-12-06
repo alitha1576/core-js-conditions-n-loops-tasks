a534fd640e4a529cccb46bae864d171e2a0bbe57
Anna Petrova <ann.undine@gmail.com>
2024-12-06 13:11:15 +0100

    solve one task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -217,8 +217,15 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let mynum = num;
+  while (mynum > 0) {
+    if (mynum % 10 === digit) {
+      return true;
+    }
+    mynum = Math.trunc(mynum / 10);
+  }
+  return false;
 }
 
 /**
@@ -234,8 +241,25 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  let leftSum = 0;
+  let rightSum = 0;
+
+  for (let i = 1; i < arr.length; i += 1) {
+    rightSum += arr[i];
+  }
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (i > 0) {
+      leftSum += arr[i - 1];
+      rightSum -= arr[i];
+    }
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
